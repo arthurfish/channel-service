c49ea493e47f19594733dc4615ed2b24553b476c
Arthur <arthurfish8051@gmail.com>
2024-12-01 15:02:17 +0800

    first commit

---
+++ src/main/kotlin/io/github/arthurfish/appender/channelservice/ChannelServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.github.arthurfish.appender.channelservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChannelServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<ChannelServiceApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/arthurfish/appender/channelservice/ChannelServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.arthurfish.appender.channelservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChannelServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
